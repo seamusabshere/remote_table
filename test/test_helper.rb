e2622d3644849b7e7102640bfce1f569eeb17199
Seamus Abshere <seamus@abshere.net>
2010-09-08 13:20:23 -0500

    support for Time.parse

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require 'test/unit'
 require 'shoulda'
 require 'ruby-debug'
 require 'errata'
+require 'active_support/all'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
