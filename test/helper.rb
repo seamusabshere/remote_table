a5f12c51750cc8df369847d7fe27765cb0abd8d0
Seamus Abshere <seamus@abshere.net>
2011-09-23 10:00:27 -0500

    in with rake, out with ruby-debug

--- test/helper.rb
+++ test/helper.rb
@@ -3,7 +3,6 @@ require 'bundler'
 Bundler.setup
 require 'test/unit'
 require 'shoulda'
-require 'ruby-debug'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
